a288bfa153b57798234cc1c1b3ef9aca72796b4f
Scott Abraham <sabraham625@gmail.com>
2017-08-12 11:18:17 -0500

    Member Details

--- src/app.jsx
+++ src/app.jsx
@@ -3,12 +3,11 @@ import PropTypes from 'prop-types';
 import Header from './components/header/header';
 import MemberContainer from './components/member-view';
 import ContentContainer from './components/content-view';
-import logo from './images/logo.svg';
 
 export default class App extends React.Component {
   constructor(props) {
     super();
-    //props.onLoad();
+    props.onLoad();
   }
 
   render() {
--- src/components/member-details/member-details.jsx
+++ src/components/member-details/member-details.jsx
@@ -2,14 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './member-details.scss';
 
-class MemberDetails extends React.Component {i
+class MemberDetails extends React.Component {
   render() {
+    let location;
+    let email;
+    if (this.props.location) {
+      location = <div><i className="material-icons">&#xE0C8;</i>{this.props.location}</div>;
+    }
+
+    if (this.props.email) {
+      email = <span><a href={`mailto://${this.props.email}`}>{this.props.email}</a></span>;
+    }
+    
+    const dateJoined = new Date(this.props.joinDate);
+    const dateJoinedDisplay = dateJoined.toString().split(' ').slice(0, 4).join(' ');
+
     return (
       <div className="member-details">
         <div className="member-details-info md-body-2">
-          <div>Since: {this.props.joinDate}</div>
-          <div><i className="material-icons">&#xE0C8;</i>{this.props.location}</div>
-          <div><i className="material-icons">&#xE0BE;</i>{this.props.email}</div>
+          {email}
+          <div>Joined: {dateJoinedDisplay}</div>
+          {location}
         </div>
         <div>repos here</div>
       </div>
